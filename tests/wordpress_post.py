addb286f872c1c640d58b9bfc0be18a285274acb
hibiki <hibiki123123@gmail.com>
2023-11-20 21:38:09 +0900

    リファクタリング5555

--- tests/wordpress_post.py
+++ tests/wordpress_post.py
@@ -1,3 +1,4 @@
+import os
 import requests
 
 # モジュールをインポート
@@ -5,23 +6,20 @@ import requests
 import json
 
 # アクセス情報の設定
-AUTH_USER = 'hibiki'
-AUTH_PASS = 'uHn2 OD9s rWFI ItC1 Fs77 IdME'
+AUTH_USER = os.getenv("WORDPRESS_API_USERNAME")
+AUTH_PASS = os.getenv("WORDPRESS_API_PASSWORD")
 
 # 投稿するURLの設定
-END_POINT_URL = "https://www.tukatech.jp/techblog/wp-json/wp/v2/posts"
+END_POINT_URL = os.getenv("WORDPRESS_BASE_URL") + os.getenv("WORDPRESS_CONTENT_POST_ENTRY_POINT")
 
 # 投稿内容
 p_title = "PythonからのWP REST API経由での投稿です"
 p_content = 'PythonからのWP REST API経由での投稿です。' # javascriptは埋め込むことができなそう
 p_status = "publish"
 
-with open('assets/posted/wordpress_post.html') as fp:
-    p_content += fp.read()
-
 payload = {
             'title': p_title ,
-            'content' : p_content ,
+            'content' : 'xxx',
             'status' : p_status,
             'slug' : 'python_wp_rest_api_post'
             }
